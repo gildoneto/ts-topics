b698646a79ea57d2b06f3af9ecbd8b24fa487e5d
Gildo Neto <gildo.neto@telus.com>
2022-11-18 11:10:15 -0300

    types

---
+++ src/01-types.ts
@@ -0,0 +1,19 @@
+type Phone = {
+  title: string,
+  price: number,
+  isOffer: boolean
+};
+
+// type Phone = {};
+
+const iPhone: Phone = {
+  title: 'iPhone X',
+  price: 799,
+  isOffer: false
+};
+
+const lenovo: Phone = {
+  title: 'Lenovo Cell',
+  price: 499,
+  isOffer: true
+};
