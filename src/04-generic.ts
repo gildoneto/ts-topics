fe9e9dde68db7a6a7373c41612c10393c1479352
Gildo Neto <gildo.neto@telus.com>
2022-12-02 10:42:16 -0300

    Generics

--- src/04-generic.ts
+++ src/04-generic.ts
@@ -45,6 +45,55 @@ const mismatchedGenerics:Person<Salary> = {
   age: 50,
   wage: {
     base: 200,
-    hours: 80
+    // hours: 80
   }
 }
+
+// ============================================= //
+
+function notNull<T>(arg: T) {
+  if (arg !== null) return arg;
+  else return null;
+}
+
+notNull('Some string');
+
+// ============================================= //
+
+type Flag = '🇪🇨'| '🇨🇱'| '🇨🇦'| '🇺🇸'| '🇧🇷'| '🇮🇷'| '🇲🇽';
+type RedFruit = '🍎' | '🍒' | '🍓' | '🍉';
+
+const redFruits: RedFruit[] = ['🍎' , '🍒' , '🍓' , '🍉'];
+const flags: Flag[] = ['🇮🇷', '🇪🇨', '🇨🇱', '🇨🇦', '🇲🇽', '🇧🇷', '🇺🇸'];
+
+const numbers = [5.2, 7.1, 2.6, 6, 9.89, 4.001, 0.9];
+const allFruits = ['🍏', '🍎', '🍋', '🍒', '🍓', '🍍', '🍊', '🍌', '🍉'];
+
+function firstFive<T>(list: T[]): T[] {
+  return list.slice(0, 5);
+}
+
+// const firstFiveArrow = <T> (lista: T[]): T[] => {
+//   return lista.slice(0, 5);
+// }
+
+// console.log(firstFive(numbers));
+// console.log(firstFive(redFruits));
+// console.log(firstFive(flags));
+
+// ============================================= //
+
+// interface dataType<T> {
+//   data: T;
+//   type: string;
+// }
+
+function dataType<T>(data: T): {data: T, type: string} {
+  const result = {
+    data: data,
+    type: typeof data
+  };
+  return result;
+}
+
+console.log(dataType('Pixies 🦜').type)
